aacb9100f807af31135094aa9cf0dab0ca99bdea
Mayank Agarwal <mayankagarwal0909@endurance.com>
2021-10-30 14:18:25 +0530

    added movie details

--- app/src/main/java/com/example/popularmovies/adapters/BindingAdapter.kt
+++ app/src/main/java/com/example/popularmovies/adapters/BindingAdapter.kt
@@ -3,9 +3,12 @@ package com.example.popularmovies.adapters
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.squareup.picasso.Picasso
+import jp.wasabeef.picasso.transformations.RoundedCornersTransformation
 
 
 @BindingAdapter("imageUrl")
 fun setImageUrl(view: ImageView, posterPath: String) {
-    Picasso.get().load("https://image.tmdb.org/t/p/w500$posterPath").into(view)
+    Picasso.get().load("https://image.tmdb.org/t/p/w500$posterPath")
+        .transform(RoundedCornersTransformation(40, 5))
+        .into(view)
 }
